0e9f114ebad6a1280488b3b7f8b2a3c3fa774ab4
Md-Mudassir <mdmudassirpro@gmail.com>
2022-06-09 18:56:46 +0530

    updates to deploy to firebase

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAqr4orusVu13LQOlakAWbAbnrihKAKToE',
+  authDomain: 'studiokai-a291d.firebaseapp.com',
+  projectId: 'studiokai-a291d',
+  storageBucket: 'studiokai-a291d.appspot.com',
+  messagingSenderId: '633057980783',
+  appId: '1:633057980783:web:c52ec4af2819725411f89a',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
